8739f7393e1b268621ecd5fa536810665253a52d
EdwardChang0207 <thshguitar2015@gmail.com>
2025-07-15 15:02:38 +0800

    cls1

---
+++ 上課檔案/20250708/input&output.py
@@ -0,0 +1,12 @@
+#output
+print(10)
+print('hi',end=' ')#結尾符號 end(init:'\n')
+print(9*8+27-(2+4)*3)
+print(10, 'hi', 123+456, 'hello', sep=',')#分隔符號 seprate -> sep(init:空格)
+
+#input -> str
+'''
+kevin -> kevin
+kevin -> hi, kevin
+'''
+print('hi,', input('請輸入姓名'))
---
+++ 上課檔案/20250708/variable.py
@@ -0,0 +1,28 @@
+# name = input('請輸入姓名')
+# print('hi,', name)
+
+'''
+DataType 資料型態
+1.Numbers
+(1)整數 integer -> int:沒有小數點 int()
+(2)浮點數 float:有小數點 float()
+eg. 123(int), pi(float), 3.0(float)
+
+2.Text
+(1)字串 string -> str: ''/"" str()
+
+3.Boolean -> bool bool()
+True(1)/False(0)
+
+4.List 串列 清單 list()
+'''
+#.    0.    1.    2.   3
+l = [123, 'hi', True, []]
+print(l[2])
+
+# a = 1
+# b = 2
+# c = 3
+
+a, b, c = 1, 2, 3
+print(a,b,c)
